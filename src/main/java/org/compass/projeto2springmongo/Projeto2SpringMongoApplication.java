195305e61e76994aec6c0e4a87ee7e3d3e1b44c8
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-09-06 07:33:54 -0300

    feat: Created project

---
+++ src/main/java/org/compass/projeto2springmongo/Projeto2SpringMongoApplication.java
@@ -0,0 +1,13 @@
+package org.compass.projeto2springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto2SpringMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projeto2SpringMongoApplication.class, args);
+    }
+
+}
